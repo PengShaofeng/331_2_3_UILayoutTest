a93d38bc15a6899daf28b9bf5b979f2fc0221d9b
PengShaofeng <3274642663@qq.com>
2019-04-18 21:32:32 +0800

    test

---
+++ app/src/main/java/com/example/dell/a331_2_3_uilayouttest/Eleventh.java
@@ -0,0 +1,13 @@
+package com.example.dell.a331_2_3_uilayouttest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class Eleventh extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eleveth);
+    }
+}
